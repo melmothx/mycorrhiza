78e1e28e02360b9cd0855609296a0bc22a1d2fa9
Marco Pessotto <melmothx@gmail.com>
2023-11-22 14:15:20 +0000

    Campify the vite.config

--- front-end/vite.config.stock.js
+++ front-end/vite.config.stock.js
@@ -5,15 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        proxy: {
-            '/account': 'http://127.0.0.1:8010/',
-            '/search': 'http://127.0.0.1:8010/',
-            '/admin': 'http://127.0.0.1:8010/',
-            '/static': 'http://127.0.0.1:8010/',
-        },
-    },
-
   plugins: [
     vue(),
   ],
