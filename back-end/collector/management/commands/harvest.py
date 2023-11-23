90350d9a88b28d8a837696f7d4bc76c35318c4e5
Marco Pessotto <melmothx@gmail.com>
2023-11-23 07:23:53 +0000

    Add the --reindex option to the harvest command

--- back-end/collector/management/commands/harvest.py
+++ back-end/collector/management/commands/harvest.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
-from amwmeta.xapian import XAPIAN_DB
-from collector.models import Site
+from amwmeta.xapian import MycorrhizaIndexer, XAPIAN_DB
+from collector.models import Site, Entry
 import shutil
 import logging
 logger = logging.getLogger(__name__)
@@ -13,17 +13,32 @@ class Command(BaseCommand):
                             help="Force a full harvest")
         parser.add_argument("--site",
                             help="Select a specific site")
+        parser.add_argument("--reindex",
+                            action="store_true", # boolean
+                            help="Do not fetch from OAI-PMH, just rebuild the Xapian index")
 
 
     def handle(self, *args, **options):
+        logger.debug(options)
+
         if options['force'] and not options['site']:
             try:
                 print("Removing " + XAPIAN_DB)
                 shutil.rmtree(XAPIAN_DB)
             except FileNotFoundError:
                 pass
+
+        if options['reindex']:
+            indexer = MycorrhizaIndexer()
+            counter = 0
+            for entry in Entry.objects.all():
+                indexer.index_record(entry.indexing_data())
+                counter += 1
+                if counter % 1000 == 0:
+                    logger.debug(str(counter) + " records done")
+            return
+
         rs = Site.objects
-        logger.debug(options)
         if options['site']:
             rs = rs.filter(url__contains=options['site'])
 
