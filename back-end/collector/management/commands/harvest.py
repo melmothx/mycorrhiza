c7d5796f878141bfb6d23e44262155b3c9010394
Marco Pessotto <melmothx@gmail.com>
2023-11-26 07:59:36 +0000

    Add nuke-aliases option to harvest

--- back-end/collector/management/commands/harvest.py
+++ back-end/collector/management/commands/harvest.py
@@ -1,8 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
 from amwmeta.xapian import MycorrhizaIndexer, XAPIAN_DB
-from collector.models import Site, Entry
+from collector.models import Site, Entry, Agent
 import shutil
 import logging
+from django.db import connection
 logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
@@ -16,7 +17,9 @@ class Command(BaseCommand):
         parser.add_argument("--reindex",
                             action="store_true", # boolean
                             help="Do not fetch from OAI-PMH, just rebuild the Xapian index")
-
+        parser.add_argument("--nuke-aliases",
+                            action="store_true",
+                            help="Remove all the aliases and variant relationships (only if --force without --site)")
 
     def handle(self, *args, **options):
         logger.debug(options)
@@ -27,6 +30,11 @@ class Command(BaseCommand):
                 shutil.rmtree(XAPIAN_DB)
             except FileNotFoundError:
                 pass
+            if options['nuke_aliases']:
+                print("Cleaning aliases")
+                Agent.objects.filter(canonical_agent_id__isnull=False).update(canonical_agent=None)
+                Entry.objects.filter(canonical_entry_id__isnull=False).update(canonical_entry=None)
+                print(connection.queries)
 
         if options['reindex']:
             indexer = MycorrhizaIndexer()
