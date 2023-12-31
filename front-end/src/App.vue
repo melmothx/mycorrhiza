2a73ed1143ca10462bb4226a580e431269d992b4
Marco Pessotto <melmothx@gmail.com>
2023-12-31 07:55:04 +0000

    Connect the login/logout to a refetch

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,13 +1,25 @@
 <script>
+ import { ref } from 'vue'
  import NavBar from './components/NavBar.vue'
  import SearchPage from './components/SearchPage.vue'
  export default {
      components: { NavBar, SearchPage },
+     setup() {
+         const search_page = ref(null)
+         return {
+             search_page
+         }
+     },
+     methods: {
+         refetch_results() {
+             this.search_page.getResults(1);
+         },
+     },
  }
 </script>
 
 <template>
-  <NavBar />
-  <SearchPage />
+  <NavBar @refetch-results="refetch_results" />
+  <SearchPage ref="search_page" />
 </template>
 
