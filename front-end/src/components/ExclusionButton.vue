4e3fe3d7cea65d59a1a00007470c3bf70044a785
Marco Pessotto <melmothx@gmail.com>
2024-01-08 07:12:01 +0000

    Add modal for the excluding button

--- front-end/src/components/ExclusionButton.vue
+++ front-end/src/components/ExclusionButton.vue
@@ -10,7 +10,8 @@
      ],
      data() {
          return {
-             comment: "Trashed",
+             open: false,
+             comment: "",
          }
      },
      methods: {
@@ -29,16 +30,56 @@
              axios.post('/search/api/exclusions', params, opts)
                   .then(function(res) {
                       console.log(res.data)
+                      vm.close_dialog();
                       vm.$emit('refetchResults');
                   })
                   .catch(function(error) {
                       console.log(error)
                   });
          },
+         open_dialog() {
+             this.open = true;
+         },
+         close_dialog() {
+             this.comment = "";
+             this.open = false;
+         }
      }
  }
 </script>
 <template>
   <span class="text-sm cursor-pointer border-2 rounded-full px-2 bg-red-800 text-white font-semibold"
-        @click="set_exclusion">Trash</span>
+        @click="open_dialog">Omit</span>
+
+  <div v-if="open">
+    <div class="fixed inset-0 z-10 w-screen overflow-y-auto font-normal font-serif">
+      <div class="flex min-h-full items-center justify-center text-center p-4">
+        <div class="border rounded-lg bg-white text-left shadow">
+          <div class="bg-white px-4 pb-4 pt-5">
+            <div class="mt-3 text-center">
+              <h3 class="text-base font-semibold leading-6 text-gray-900">Omit {{ object_type }}</h3>
+              <div class="mt-2">
+                <textarea v-model="comment" placeholder="Reason"
+                          class="rounded border-gray-300 focus:ring-0 active:ring-0 active:border-pink-300 focus:border-pink-300">
+                </textarea>
+              </div>
+            </div>
+          </div>
+          <div class="bg-gray-50 px-4 py-3 flex">
+            <button type="button" class="inline-flex w-full justify-center rounded-md border m-1"
+                    @click="close_dialog">
+              Cancel
+            </button>
+            <button v-if="comment"
+                    type="button" class="inline-flex w-full justify-center rounded-md border m-1"
+                    @click="set_exclusion">
+              Omit
+            </button>
+          </div>
+        </div>
+        <div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
