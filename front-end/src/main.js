861ba10b1536df4410f5e1a0dfccde1addb0d396
Marco Pessotto <melmothx@gmail.com>
2024-03-01 15:35:52 +0000

    Bootstrap the i18n

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,10 +1,22 @@
 // import './assets/main.css'
 import "./style.css"
 import { createApp } from 'vue'
+import { createGettext } from "vue3-gettext";
+import translations from "./i18n/translations.json";
 
 import Collector from './Collector.vue'
 import router from './router'
 
 const app = createApp(Collector)
+
 app.use(router)
+app.use(createGettext({
+    availableLanguages: {
+        en: "English",
+        it: "Italiano",
+    },
+    defaultLanguage: "en",
+    translations: translations,
+}))
+
 app.mount('#app')
