fbb9ef5af3e9f43ac020e064c58991ed7ee44706
Marco Pessotto <melmothx@gmail.com>
2024-03-04 10:12:56 +0000

    Fix the update-i18n.sh script

--- utils/bin/update-i18n.sh
+++ utils/bin/update-i18n.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 set -e
+set -x
+i18ndir=src/i18n
 
 cd $(dirname $0)
 cd ../../front-end
-../utils/bin/create_pot.pl src --pot src/i18n/messages.pot
+../utils/bin/create_pot.pl src --pot "$i18ndir/messages.pot"
+
+for i in en it de fr; do
+    if [ ! -f "$i18ndir/$i.po" ]; then
+        msginit --input="$i18ndir/messages.pot" --output "$i18ndir/$i.po" --locale=$i
+    fi
+    msgmerge --no-fuzzy-matching --update "$i18ndir/$i.po" "$i18ndir/messages.pot"
+done
