8421c38e0e37dded0da9846e3188b64f4ed55be9
Marco Pessotto <melmothx@gmail.com>
2024-09-11 08:27:36 +0000

    Make the navbar usable on mobile

--- front-end/src/components/NavBar.vue
+++ front-end/src/components/NavBar.vue
@@ -32,6 +32,7 @@
              message: null,
              reset_message: null,
              current_language: null,
+             show_login: false,
          }
      },
      methods: {
@@ -47,42 +48,40 @@
                   });
          },
          login() {
-             const vm = this;
-             vm.message = "";
-             vm.reset_message = null;
+             this.message = "";
+             this.reset_message = null;
              axios.post('/collector/api/auth/login', {
                  "username": this.username,
                  "password": this.password,
-             }).then(function(res) {
+             }).then((res) => {
                  if (res.data.logged_in) {
-                     vm.authenticated = res.data.logged_in;
-                     vm.user_data = res.data
-                     vm.message = null;
-                     vm.$emit('refetchResults');
+                     this.authenticated = res.data.logged_in;
+                     this.user_data = res.data
+                     this.message = null;
+                     this.$emit('refetchResults');
                  }
                  else {
-                     vm.message = res.data.error || "Failed login";
+                     this.message = res.data.error || "Failed login";
                  }
              });
          },
          password_reset() {
-             const vm = this;
-             vm.message = "";
+             this.message = "";
              axios.post('/collector/api/auth/reset-password', {
                  "operation": "send-link",
                  "username":  this.username,
-             }).then(function(res) {
+             }).then((res) => {
                  console.log(res.data.message);
-                 vm.message = "";
-                 vm.reset_message = res.data.message;
+                 this.message = "";
+                 this.reset_message = res.data.message;
              });
          },
          logout() {
-             const vm = this;
              axios.get('/collector/api/auth/logout')
-                  .then(function(res) {
-                      vm.$emit('refetchResults');
-                      vm.check();
+                  .then((res) => {
+                      this.show_login = false;
+                      this.$emit('refetchResults');
+                      this.check();
                   });
          },
          set_language() {
@@ -111,36 +110,13 @@
       <span class="px-3">{{ $gettext('Hello, %1!', authenticated) }}</span>
     </div>
     <div v-else>
-      <form @submit.prevent="login" class="ml-4">
-        <input class="outline outline-0 border border-gray-300 focus:border-spectra-500 focus:ring-0 px-2 rounded-none h-8 w-32"
-               :placeholder="$gettext('Username')"
-               type="text" v-model="username" required>
-        <input class="outline outline-0 border border-gray-300 focus:border-spectra-500 focus:ring-0 px-2 h-8 w-32"
-               :placeholder="$gettext('Password')"
-               type="password" v-model="password" required>
-        <button class="h-8 btn-secondary rounded-none rounded-br-3xl pr-10 pl-4 italic font-normal h-8 mr-2"
-                type="submit">{{ $gettext('Login') }}</button>
-      </form>
-      <div v-if="message" class="pt-2">
-        <div class="ml-4 py-2 text-claret-900 font-bold">
-          {{ $gettext(message) }}
-        </div>
-        <form v-if="username" @submit.prevent="password_reset" class="ml-4">
-          <button id="reset-password"
-                  class="h-8 btn-secondary rounded-none rounded-br-3xl pr-10 pl-4 italic font-normal h-8 mr-2"
-                  type="submit">{{ $gettext('Reset Password for %1?', username) }}</button>
-        </form>
-      </div>
-      <div v-if="reset_message" class="p-2 ml-4 text-claret-900 font-bold" @click="reset_message = null">
-        {{ $gettext(reset_message) }}
-      </div>
+      <UserIcon class="h-4 w-4 m-2 text-spectra-800 cursor-pointer" @click="show_login = show_login ? false : true" />
     </div>
-
     <div v-if="authenticated">
       <Menu as="div" class="relative z-10">
         <div>
           <MenuButton class="inline-flex w-full justify-center text-spectra-800 h-8 p-1">
-            <UserIcon class="h-5 w-5" title="User Menu" />
+            <UserIcon class="h-5 w-5" :title="$gettext('User Menu')" />
             <ChevronUpDownIcon class="h-5 w-5" aria-hidden="true"/>
           </MenuButton>
         </div>
@@ -214,6 +190,31 @@
           </transition>
         </div>
       </Listbox>
+    </div>
+  </div>
+  <div v-if="!authenticated && show_login">
+    <form @submit.prevent="login" class="ml-4 flex">
+      <input class="outline outline-0 border border-gray-300 focus:border-spectra-500 focus:ring-0 px-2 rounded-none h-8 w-32"
+             :placeholder="$gettext('Username')"
+             type="text" v-model="username" required>
+      <input class="outline outline-0 border border-gray-300 focus:border-spectra-500 focus:ring-0 px-2 h-8 w-32"
+             :placeholder="$gettext('Password')"
+             type="password" v-model="password" required>
+      <button class="h-8 btn-secondary rounded-none rounded-br-3xl pr-10 pl-4 italic font-normal h-8 mr-2"
+              type="submit">{{ $gettext('Login') }}</button>
+    </form>
+    <div v-if="message" class="pt-2">
+      <div class="ml-4 py-2 text-claret-900 font-bold">
+        {{ $gettext(message) }}
+      </div>
+      <form v-if="username" @submit.prevent="password_reset" class="ml-4">
+        <button id="reset-password"
+                class="h-8 btn-secondary rounded-none rounded-br-3xl pr-10 pl-4 italic font-normal h-8 mr-2"
+                type="submit">{{ $gettext('Reset Password for %1?', username) }}</button>
+      </form>
+    </div>
+    <div v-if="reset_message" class="p-2 ml-4 text-claret-900 font-bold" @click="reset_message = null">
+      {{ $gettext(reset_message) }}
     </div>
   </div>
 </template>
