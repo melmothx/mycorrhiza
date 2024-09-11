0a727efefa347d09959726dfad0c95946404cbd9
Marco Pessotto <melmothx@gmail.com>
2024-09-11 08:35:14 +0000

    Improve navbar

--- front-end/src/components/NavBar.vue
+++ front-end/src/components/NavBar.vue
@@ -106,11 +106,42 @@
     </div>
     <div class="flex-grow">
     </div>
+    <div :title="$gettext('UI language')">
+      <Listbox v-model="current_language">
+        <div class="relative m-0">
+          <ListboxButton class="relative w-full cursor-pointer py-1 h-8 pl-3 pr-10 text-left shadow-md text-sm bg-perl-bush-50 rounded"
+                         v-slot="{ open }">
+            <span class="block truncate font-bold">{{ current_language }}</span>
+            <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon
+                  class="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+              />
+            </span>
+          </ListboxButton>
+          <transition
+              enter-active-class="transition duration-100 ease-out"
+              enter-from-class="transform scale-95 opacity-0"
+              enter-to-class="transform scale-100 opacity-100"
+              leave-active-class="transition duration-75 ease-in"
+              leave-from-class="transform scale-100 opacity-100"
+              leave-to-class="transform scale-95 opacity-0">
+            <ListboxOptions class="absolute mt-1 max-h-60 w-full overflow-auto bg-perl-bush-50 pl-3 text-base shadow-lg rounded">
+              <ListboxOption v-for="lang in ['en', 'it']"
+                             :value="lang" :key="lang"
+                             @click="set_language"
+                             class="cursor-pointer hover:text-spectra-800"
+              >{{ lang }}</ListboxOption>
+            </ListboxOptions>
+          </transition>
+        </div>
+      </Listbox>
+    </div>
     <div v-if="authenticated">
       <span class="px-3">{{ $gettext('Hello, %1!', authenticated) }}</span>
     </div>
     <div v-else>
-      <UserIcon class="h-4 w-4 m-2 text-spectra-800 cursor-pointer" @click="show_login = show_login ? false : true" />
+      <UserIcon class="h-5 w-5 m-2 text-spectra-800 cursor-pointer" @click="show_login = show_login ? false : true" />
     </div>
     <div v-if="authenticated">
       <Menu as="div" class="relative z-10">
@@ -160,44 +191,13 @@
         </transition>
       </Menu>
     </div>
-    <div :title="$gettext('UI language')">
-      <Listbox v-model="current_language">
-        <div class="relative m-0">
-          <ListboxButton class="relative w-full cursor-pointer py-1 h-8 pl-3 pr-10 text-left shadow-md text-sm bg-perl-bush-50 rounded"
-                         v-slot="{ open }">
-            <span class="block truncate font-bold">{{ current_language }}</span>
-            <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-              <ChevronUpDownIcon
-                  class="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-              />
-            </span>
-          </ListboxButton>
-          <transition
-              enter-active-class="transition duration-100 ease-out"
-              enter-from-class="transform scale-95 opacity-0"
-              enter-to-class="transform scale-100 opacity-100"
-              leave-active-class="transition duration-75 ease-in"
-              leave-from-class="transform scale-100 opacity-100"
-              leave-to-class="transform scale-95 opacity-0">
-            <ListboxOptions class="absolute mt-1 max-h-60 w-full overflow-auto bg-perl-bush-50 pl-3 text-base shadow-lg rounded">
-              <ListboxOption v-for="lang in ['en', 'it']"
-                             :value="lang" :key="lang"
-                             @click="set_language"
-                             class="cursor-pointer hover:text-spectra-800"
-              >{{ lang }}</ListboxOption>
-            </ListboxOptions>
-          </transition>
-        </div>
-      </Listbox>
-    </div>
   </div>
   <div v-if="!authenticated && show_login">
-    <form @submit.prevent="login" class="ml-4 flex">
-      <input class="outline outline-0 border border-gray-300 focus:border-spectra-500 focus:ring-0 px-2 rounded-none h-8 w-32"
+    <form @submit.prevent="login" class="mx-4 flex">
+      <input class="outline outline-0 border border-gray-300 focus:border-spectra-500 focus:ring-0 px-2 rounded-none h-8 w-full"
              :placeholder="$gettext('Username')"
              type="text" v-model="username" required>
-      <input class="outline outline-0 border border-gray-300 focus:border-spectra-500 focus:ring-0 px-2 h-8 w-32"
+      <input class="outline outline-0 border border-gray-300 focus:border-spectra-500 focus:ring-0 px-2 h-8 w-full"
              :placeholder="$gettext('Password')"
              type="password" v-model="password" required>
       <button class="h-8 btn-secondary rounded-none rounded-br-3xl pr-10 pl-4 italic font-normal h-8 mr-2"
