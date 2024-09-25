6e1a138d56ee64578da10e35ae17ed033e9e3f5b
Marco Pessotto <melmothx@gmail.com>
2024-09-25 14:25:04 +0000

    Front end part for the library badges

--- front-end/src/components/LibraryBadges.vue
+++ front-end/src/components/LibraryBadges.vue
@@ -1,24 +1,46 @@
 <script>
+ import Axios from 'axios';
+ import { setupCache } from 'axios-cache-interceptor';
+ const instance = Axios.create();
+ const caxios = setupCache(instance)
  export default {
      props: [ 'data_sources' ],
-     computed: {
-         libraries() {
-             return [ ...new Map(this.data_sources.map(v => [ v.library_id,
-                                                              { id: v.library_id,
-                                                                name: v.library_name
-                                                              }
-             ])).values() ]
+     data() {
+         return {
+             libraries: [],
          }
-     }
+     },
+     methods: {
+         get_library_details() {
+             /* cached request */
+             caxios.get('/collector/api/libraries')
+                   .then(res => {
+                       const libs = {};
+                       res.data.libraries.forEach((l, i) => {
+                           libs[l.id] = l;
+                       });
+                       this.libraries =
+                           [ ...new Map(this.data_sources.map(v => [ v.library_id,
+                                                                     { id: v.library_id,
+                                                                       details: libs[v.library_id],
+                                                                     } ])).values() ];
+                   })
+                   .catch(error => {
+                       console.log(error);
+                   });
+         }
+     },
+     mounted() {
+         this.get_library_details();
+     },
  }
 </script>
 <template>
   <div class="mt-1 mx-2 flex flex-wrap">
     <span v-for="l in libraries">
-      <router-link class="border border-spectra-500 whitespace-nowrap
-                          rounded text-spectra-600 hover:text-spectra-900 text-[12px] p-px m-1 leading-8"
+      <router-link :class="`mcrz-micro-badge mcrz-library-${l.details.library_type || 'digital'}`"
                    :to="{ name: 'library_view', params: { id: l.id } }">
-        {{ l.name }}
+        {{ l.details.name }}
       </router-link>
     </span>
   </div>
