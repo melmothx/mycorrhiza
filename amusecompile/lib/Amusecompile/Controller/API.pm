b6f4d876f2580af9f2b1372b1b977a1c4c37dd62
Marco Pessotto <melmothx@gmail.com>
2024-11-23 09:35:33 +0000

    Implement the file upload

--- amusecompile/lib/Amusecompile/Controller/API.pm
+++ amusecompile/lib/Amusecompile/Controller/API.pm
@@ -1,6 +1,7 @@
 package Amusecompile::Controller::API;
 use Path::Tiny ();
 use Mojo::Base 'Mojolicious::Controller', -signatures;
+use Data::Dumper::Concise;
 
 
 sub check ($self) {
@@ -8,14 +9,61 @@ sub check ($self) {
     $self->render(json => { status => 'OK' });
 }
 
-sub create_session($self) {
+sub create_session ($self) {
     my $wd = $self->wd;
     $wd->mkdir;
     my $sessiondir = $self->wd->tempdir(CLEANUP => 0);
     $self->log->debug("Creating session");
     my $sid = $sessiondir->basename;
     $self->pg->db->query('INSERT INTO amc_sessions (sid, created, last_modified) VALUES(?, NOW(), NOW())', $sid);
-    $self->render(json => { session => $sessiondir->basename });
+    $self->render(json => { session_id => $sid });
+}
+
+sub add_file ($self) {
+    my $sid = $self->param('sid');
+    if ($self->req->is_limit_exceeded) {
+        return $self->render(json => { status => 'File is too big.' });
+    }
+    my $out = {};
+    if ($sid) {
+        my $db = $self->pg->db;
+        if (my $check = $db->query('SELECT sid FROM amc_sessions WHERE sid = ?', $sid)->hash) {
+            if ($check and $check->{sid}) {
+                my $stack_sql = 'SELECT COUNT(*) AS idx, SUM(file_size) AS total_size FROM amc_session_files WHERE sid = ?';
+                my $stack = $db->query($stack_sql, $sid)->hash;
+                my $index = $stack->{idx} + 1;
+                $self->log->debug(Dumper($stack));
+                if ($stack->{total_size} > (1024 * 1024 * 64)) {
+                    return $self->render(json => { status => 'Quota exceeded' });
+                }
+                if (my $upload = $self->param('muse')) {
+                    my $basename = sprintf('%03d.zip', $index);
+                    my $destination = $self->wd->child($sid)->child($basename);
+                    $upload->move_to($destination);
+                    $self->log->info("Uploaded $destination");
+                    my $sql =<<'SQL';
+INSERT INTO amc_session_files
+       (sid, basename, original_filename, sorting_index, file_size, created, last_modified)
+VALUES (?,   ?,        ?,                 ?,             ?,         NOW(),    NOW()       )
+RETURNING id
+SQL
+                    my $inserted = $db->query($sql,
+                                              $sid, $basename, $upload->filename, $index, $upload->size)
+                      ->hash->{id};
+                    $out->{success} = 1;
+                    $out->{status} = 'OK';
+                    $out->{file_id} = $inserted;
+                }
+            }
+        }
+        else {
+            $out->{status} = 'Invalid session id';
+        }
+    }
+    else {
+        $out->{status} = 'Missing session id';
+    }
+    $self->render(json => $out);
 }
 
 1;
--- amusecompile/migrations.sql
+++ amusecompile/migrations.sql
@@ -6,9 +6,12 @@ CREATE TABLE amc_sessions (
 );
 
 CREATE TABLE amc_session_files (
+   id SERIAL NOT NULL PRIMARY KEY,
    sid VARCHAR(64) NOT NULL REFERENCES amc_sessions(sid) ON DELETE CASCADE ON UPDATE CASCADE,
    basename VARCHAR(255) NOT NULL,
+   original_filename VARCHAR(255) NOT NULL,
    sorting_index INTEGER NOT NULL,
+   file_size INTEGER NOT NULL,
    created TIMESTAMP WITH TIME ZONE NOT NULL,
    last_modified TIMESTAMP WITH TIME ZONE NOT NULL
 );
