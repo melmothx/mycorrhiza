e59b57e2b07d5d80c4418e3484be35e90b569dc5
Marco Pessotto <melmothx@gmail.com>
2024-12-14 14:03:07 +0000

    Implement tab system

--- front-end/src/components/BookBuilderView.vue
+++ front-end/src/components/BookBuilderView.vue
@@ -12,10 +12,14 @@
      },
      data() {
          return {
-             bookbuilder
+             bookbuilder,
+             current_tab: "general",
          }
      },
      methods: {
+         set_tab(tab) {
+             this.current_tab = tab;
+         },
          drop_element(event, id) {
              const move_id = event.dataTransfer.getData('move_id');
              console.log(`Dropping ${move_id} into ${id}`);
@@ -115,44 +119,79 @@
  }
 </script>
 <template>
-  <div v-for="text in bookbuilder.text_list" :key="text.sid + text.id">
-    <div class="flex my-2">
-      <TrashIcon class="h-6 w-6 text-cab-sav-800" @click="remove_element(text.id)" />
-      <div @drop="drop_element($event, text.id)"
+  <div class="font-medium text-center text-gray-500 mb-8">
+    <ul class="flex flex-wrap">
+      <li>
+        <a href="#" @click="set_tab('general')" :class="current_tab == 'general' ? 'mcrz-tab-active' : 'mcrz-tab-normal'">
+          General
+        </a>
+      </li>
+      <li>
+        <a href="#" @click="set_tab('layout')" :class="current_tab == 'layout' ? 'mcrz-tab-active' : 'mcrz-tab-normal'">
+          Layout
+        </a>
+      </li>
+      <li>
+        <a href="#" @click="set_tab('fonts')" :class="current_tab == 'fonts' ? 'mcrz-tab-active' : 'mcrz-tab-normal'">
+          Fonts
+        </a>
+      </li>
+      <li>
+        <a href="#" @click="set_tab('imposition')" :class="current_tab == 'imposition' ? 'mcrz-tab-active' : 'mcrz-tab-normal'">
+          Imposition
+        </a>
+      </li>
+      <li>
+        <a href="#" @click="set_tab('advanced')" :class="current_tab == 'advanced' ? 'mcrz-tab-active' : 'mcrz-tab-normal'">
+          Advanced
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div id="bb-tabs">
+    <div v-if="current_tab == 'general'" id="bb-general">
+      <div v-for="text in bookbuilder.text_list" :key="text.sid + text.id">
+        <div class="flex my-2">
+          <TrashIcon class="h-6 w-6 text-cab-sav-800" @click="remove_element(text.id)" />
+          <div @drop="drop_element($event, text.id)"
          @dragover.prevent @dragenter.prevent
          @dragstart="drag_element($event, text.id)"
          draggable="true" class="font-bold cursor-grab">{{ text.id }} {{ text.title }}</div>
+        </div>
+      </div>
     </div>
   </div>
-  <div v-if="bookbuilder.can_be_compiled()">
-    <button class="btn-accent m-1 px-4 py-1 rounded shadow-lg" @click="build">
-      {{ $gettext('Build') }}
-    </button>
-  </div>
-  <div v-if="bookbuilder.status == 'finished'">
-    <a :href="download_url()" class="btn-primary m-1 px-4 py-1 rounded shadow-lg">
-      {{ $gettext('Download') }}
-    </a>
-  </div>
-  <div v-if="bookbuilder.status == 'failed'">
-    <button class="btn-primary m-1 px-4 py-1 rounded shadow-lg">
-      {{ $gettext('Failed') }}
-    </button>
-  </div>
-  <div v-if="bookbuilder.status == 'inactive'">
-    <button class="btn-accent m-1 px-4 py-1 rounded shadow-lg">
-      <span class="flex items-center">
-        <ArrowPathIcon class="h-4 w-4 mr-1 animate-spin" />
-        {{ $gettext('Queued') }}
-      </span>
-    </button>
-  </div>
-  <div v-if="bookbuilder.status == 'active'">
-    <button type="button" class="btn-accent m-1 px-4 py-1 rounded shadow-lg">
-      <span class="flex items-center">
-        <Cog8ToothIcon class="h-4 w-4 mr-1 animate-spin" />
-        {{ $gettext('Working') }}
-      </span>
-    </button>
+  <div id="bb-action" class="text-center mt-8">
+    <div v-if="bookbuilder.can_be_compiled()">
+      <button class="btn-accent m-1 px-4 py-1 rounded shadow-lg" @click="build">
+        {{ $gettext('Build') }}
+      </button>
+    </div>
+    <div v-if="bookbuilder.status == 'finished'">
+      <a :href="download_url()" class="btn-primary m-1 px-4 py-1 rounded shadow-lg">
+        {{ $gettext('Download') }}
+      </a>
+    </div>
+    <div v-if="bookbuilder.status == 'failed'">
+      <button class="btn-primary m-1 px-4 py-1 rounded shadow-lg">
+        {{ $gettext('Failed') }}
+      </button>
+    </div>
+    <div v-if="bookbuilder.status == 'inactive'">
+      <button class="btn-accent m-1 px-4 py-1 rounded shadow-lg">
+        <span class="flex items-center">
+          <ArrowPathIcon class="h-4 w-4 mr-1 animate-spin" />
+          {{ $gettext('Queued') }}
+        </span>
+      </button>
+    </div>
+    <div v-if="bookbuilder.status == 'active'">
+      <button type="button" class="btn-accent m-1 px-4 py-1 rounded shadow-lg">
+        <span class="flex items-center">
+          <Cog8ToothIcon class="h-4 w-4 mr-1 animate-spin" />
+          {{ $gettext('Working') }}
+        </span>
+      </button>
+    </div>
   </div>
 </template>
