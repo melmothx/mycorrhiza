139204b88071b786f8f29963f292e919586e43d2
Marco Pessotto <melmothx@gmail.com>
2025-05-08 13:38:00 +0000

    Add a search input in the library pages

--- front-end/src/components/LibraryOverView.vue
+++ front-end/src/components/LibraryOverView.vue
@@ -10,7 +10,9 @@
      data() {
          return {
              libraries: [],
+             filtered_libraries: [],
              error: null,
+             query: null,
          }
      },
      methods: {
@@ -18,11 +20,26 @@
              axios.get('/collector/api/libraries')
                   .then(res => {
                       this.error = null;
-                      this.libraries = res.data.libraries;
+                      this.libraries = this.filtered_libraries = res.data.libraries;
                   })
                   .catch(error => {
                       this.error = error;
                   });
+         },
+         filter_libraries() {
+             console.log(`Search ${this.query}`);
+             const search = this.query.normalize("NFKD").replace(/\p{Mn}/gu, "").toLowerCase();
+             this.filtered_libraries = this.libraries.filter((l) => {
+                 for (const k in l) {
+                     if (typeof l[k] === "string") {
+                         if (l[k].normalize("NFKD").replace(/\p{Mn}/gu, "")
+                                 .toLowerCase().includes(search)) {
+                             return true;
+                         }
+                     }
+                 }
+                 return false;
+             })
          },
      },
      mounted() {
@@ -36,8 +53,14 @@
     {{ $gettext(error) }}
   </div>
   <h1 class="font-bold text-4xl text-center my-4">{{ $gettext('Libraries') }}</h1>
+  <div class="sm:flex sm:flex-nowrap sm:flex-nowrap sm:h-8 my-8">
+    <input class="mcrz-input shadow-sm w-full my-1 sm:my-0"
+           v-model="query"
+           @input="filter_libraries"
+           :placeholder="$gettext('Type here to search the libraries')">
+  </div>
   <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
-    <div class="m-1" v-for="library in libraries">
+    <div class="m-1" v-for="library in filtered_libraries">
       <LibraryBox :library="library">
         <router-link class="font-bold"
                      :to="{ name: 'library_view', params: { id: library.id } }">
