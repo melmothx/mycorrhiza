8cceb0783dca24c29d70dababe86425211daa486
Marco Pessotto <melmothx@gmail.com>
2025-09-05 19:46:57 +0000

    Fix crash when pulling the spreadsheet form

--- back-end/collector/views.py
+++ back-end/collector/views.py
@@ -1071,7 +1071,7 @@ def api_spreadsheet(request, library_id):
     except Library.DoesNotExist:
         out['error'] = "Library does not exist"
     if library:
-        if user.is_superuser or library.affiliated_users.filter(library_admin=True, user=user).count():
+        if user.is_superuser or library.affiliated_users.filter(library_admin=True, username=user.username).count():
             sites = [ { "title": s.title, "id": s.id } for s in library.sites.filter(site_type="csv", active=True) ]
     if sites:
         if request.method == 'POST':
