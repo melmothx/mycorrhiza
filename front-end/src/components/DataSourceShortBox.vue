9a7a356d8a087cf5844086594c78f50d5d501dd9
Marco Pessotto <melmothx@gmail.com>
2025-11-02 08:23:52 +0000

    Start implementation of the recursive aggregation in FE

---
+++ front-end/src/components/DataSourceShortBox.vue
@@ -0,0 +1,28 @@
+<script>
+ export default {
+     props: [ 'source' ]
+ }
+</script>
+<template>
+  <div v-if="source.authors && source.authors.length" class="border-old-copper-100 flex flex-wrap">
+    <div v-for="author in source.authors" :key="author" class="me-2 my-2 p-1 border border-old-copper-200 rounded">
+      {{ author }}
+    </div>
+  </div>
+  <div class="flex">
+    <div class="grow">
+      <h3 class="font-semibold mr-1 p-1 grow" v-if="source.title">
+        {{ source.title }}
+      </h3>
+      <h4 class="mr-1 p-1 grow" v-if="source.subtitle">
+        <em>{{ source.subtitle }}</em>
+      </h4>
+    </div>
+    <div class="font-bold" v-if="source.languages">
+      <span class="bg-linear-to-tr from-spectra-700 to-spectra-900 rounded-sm p-1 shadow-md ml-2 text-white"
+            v-for="lang in source.languages" :key="lang">
+        {{ lang }}
+      </span>
+    </div>
+  </div>
+</template>
--- front-end/src/components/EntryDetails.vue
+++ front-end/src/components/EntryDetails.vue
@@ -5,24 +5,27 @@
 </script>
 <template>
   <h2><slot></slot></h2>
-  <div v-if="record.authors">
-    <div v-for="author in record.authors" :key="author.id">
-      <a :href="`/library/author/${author.id}`">
+  <div v-if="record.authors" class="border-old-copper-100 flex flex-wrap">
+    <div v-for="author in record.authors" :key="author.id"
+         class="me-2 my-1 p-1 border border-old-copper-200 rounded hover:bg-perl-bush-50">
+      <a :href="`/library/author/${author.id}`" class="">
         {{ author.value }}
       </a>
     </div>
   </div>
-  <div class="flex">
-    <h3 class="font-semibold mr-1" v-if="record.title">
-      {{ record.title }}
-    </h3>
-    <div v-if="record.languages">
-      <div v-for="l in record.languages" :key="l.id">
-        ({{ l.value }})
+  <div class="mb-4">
+    <div class="flex mt-4 text-lg">
+      <h3 class="font-semibold mr-1" v-if="record.title">
+        {{ record.title }}
+      </h3>
+      <div v-if="record.languages">
+        <div v-for="l in record.languages" :key="l.id">
+          ({{ l.value }})
+        </div>
       </div>
     </div>
+    <h4 class="italic" v-if="record.subtitle">
+      {{ record.subtitle }}
+    </h4>
   </div>
-  <h4 class="italic" v-if="record.subtitle">
-    {{ record.subtitle }}
-  </h4>
 </template>
