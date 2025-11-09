c03351edafe862e7033ccd430a773a7505ca7f2b
Marco Pessotto <melmothx@gmail.com>
2025-11-09 06:38:03 +0000

    Adjust entry page

--- front-end/src/components/EntryDetails.vue
+++ front-end/src/components/EntryDetails.vue
@@ -4,28 +4,27 @@
   }
 </script>
 <template>
-  <h2><slot></slot></h2>
-  <div v-if="record.authors" class="border-old-copper-100 flex flex-wrap">
-    <div v-for="author in record.authors" :key="author.id"
-         class="me-2 my-1 p-1 border border-old-copper-200 rounded hover:bg-perl-bush-50">
-      <a :href="`/library/author/${author.id}`" class="">
-        {{ author.value }}
-      </a>
-    </div>
-  </div>
-  <div class="mb-4">
-    <div class="flex mt-1 text-lg">
-      <h3 class="font-semibold mr-1" v-if="record.title">
+  <div class="mb-4 pb-2">
+    <div class="mb-2">
+      <h3 class="font-semibold mt-1 text-lg">
         {{ record.title }}
+        <span v-if="record.languages">
+          <span v-for="l in record.languages" :key="l.id">
+            ({{ l.value }})
+          </span>
+        </span>
       </h3>
-      <div v-if="record.languages">
-        <div v-for="l in record.languages" :key="l.id">
-          ({{ l.value }})
-        </div>
+      <h4 class="italic" v-if="record.subtitle">
+        {{ record.subtitle }}
+      </h4>
+    </div>
+    <div v-if="record.authors" class="border-old-copper-100 flex flex-wrap">
+      <div v-for="author in record.authors" :key="author.id"
+           class="me-2 my-1 p-1 border border-old-copper-200 rounded hover:bg-perl-bush-50">
+        <a :href="`/library/author/${author.id}`" class="">
+          {{ author.value }}
+        </a>
       </div>
     </div>
-    <h4 class="italic" v-if="record.subtitle">
-      {{ record.subtitle }}
-    </h4>
   </div>
 </template>
