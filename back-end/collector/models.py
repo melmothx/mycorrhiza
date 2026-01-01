melmothx/mycorrhiza

commit 451c8da5096e9a4ea2d941250b54f202b9312185

When indexing generic recoreds, reindex the variants/canonicals

back-end/collector/models.py
@@ -623,7 +623,10 @@ class Site(models.Model):
             record['full_data'] = full
             record['deleted'] = False
             for entry in self.process_harvested_record(record, aliases, now):
-                xapian_records.append(entry.id)
+                if entry.canonical_entry:
+                    xapian_records.append(entry.canonical_entry.id)
+                for e in entry.variant_entries.all():
+                    xapian_records.append(e.id)
         return self.index_harvested_records(xapian_records, now=now)
 
     def process_calibre_tree(self, force):

back-end/amwmeta/spip.py
@@ -120,10 +120,17 @@ class SpipIndexer:
                                         }
                                     })
                         if self.body_is_description:
-                            xpath = "//*[contains(concat(' ', normalize-space(@class), ' '), ' {}-{} ')]"
+                            xpath = "//*[contains(concat(' ', normalize-space(@class), ' '), ' {} ')]"
                             full_body = []
-                            for spip_class in ('article-chapo', 'article-texte', 'article-descriptif', 'article-ps'):
-                                for el in doc.xpath(xpath.format(spip_class, identifier)):
+                            crawled_classes = (
+                                "article-chapo-{}".format(identifier),
+                                "article-texte-{}".format(identifier),
+                                "article-descriptif-{}".format(identifier),
+                                "divers",
+                                "article-ps-{}".format(identifier),
+                            )
+                            for spip_class in crawled_classes:
+                                for el in doc.xpath(xpath.format(spip_class)):
                                     fragment = extract_text_from_element(el, url)
                                     if fragment['body']:
                                         full_body.append(fragment['body'])

commit aeeb6afe4eab2f41d25e1b7caa7657bdd4ead1e0

Fix bug introduced in 451c8da

back-end/collector/models.py
@@ -623,6 +623,7 @@ class Site(models.Model):
             record['full_data'] = full
             record['deleted'] = False
             for entry in self.process_harvested_record(record, aliases, now):
+                xapian_records.append(entry.id)
                 if entry.canonical_entry:
                     xapian_records.append(entry.canonical_entry.id)
                 for e in entry.variant_entries.all():